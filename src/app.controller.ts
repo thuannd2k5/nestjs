7a66d7d50e839228f4e500d2fc75393eb8961368
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-14 03:07:21 +0700

    sử dụng jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,19 +2,20 @@ import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/commo
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
-
+    private authService: AuthService
   ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
 }
