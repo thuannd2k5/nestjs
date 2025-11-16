83bdddcb30e064eb6859fd58e8eb9ce24aef0044
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-17 00:27:07 +0700

    Implementing Passport JWT( thực hiện khi các router muốn sử dụng thì phải cần token để truy cập)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -16,6 +17,12 @@ export class AppController {
   @Post('/login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 
 }
