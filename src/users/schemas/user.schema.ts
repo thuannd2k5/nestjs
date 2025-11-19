fff0c9e2008cd22581bcf70e5abdcaab1f98767c
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-20 03:20:41 +0700

    plugin timestamp

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -4,7 +4,7 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
     @Prop({ required: true })
     email: string;
