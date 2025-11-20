81119925cd3b92f0b869beea9440cff2fa4ff30a
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-20 22:41:20 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-
+  //config cors
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false
+    }
+  );
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
