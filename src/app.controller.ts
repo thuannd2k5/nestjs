caf81945bf5dce453ea6029d20b8fd1a9bb11464
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-21 09:01:14 +0700

    Update user type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,7 @@
 import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
-
 @Controller()
 export class AppController {
   constructor(
@@ -13,18 +9,5 @@ export class AppController {
     private configService: ConfigService,
     private authService: AuthService
   ) { }
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  handleLogin(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  // @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 
 }
--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -27,7 +27,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     handleRequest(err, user, info) {
         // You can throw an exception based on either "info" or "err" arguments
         if (err || !user) {
-            throw err || new UnauthorizedException("Token khong hop le");
+            throw err || new UnauthorizedException("Token khong hop le or khong co bearer token ở header");
         }
         return user;
     }
