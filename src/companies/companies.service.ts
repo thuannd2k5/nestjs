3f88930b078591c85a82bfd3da1cdc9278a7e096
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-22 23:31:05 +0700

    fix lỗi và done phần pagination

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -25,7 +25,7 @@ export class CompaniesService {
   }
 
   async findAll(currentPage: number, limit: number, qs: string) {
-    const { filter, sort, projection, population } = aqp(qs);
+    const { filter, sort, population } = aqp(qs);
     delete filter.page;
     delete filter.limit;
 
@@ -37,8 +37,7 @@ export class CompaniesService {
     const result = await this.companyModel.find(filter)
       .skip(offset)
       .limit(defaultLimit)
-      // @ts-ignore: Unreachable code error
-      .sort(sort)
+      .sort(sort as any)
       .populate(population)
       .exec();
 
