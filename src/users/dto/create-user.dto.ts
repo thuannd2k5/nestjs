caf5a9717ec8969e5722cb13591fb65020060731
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-23 14:22:41 +0700

    update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,65 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
 
+class Company {
+    @IsNotEmpty()
+    id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty()
+    name: string;
+}
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
+
+    @IsNotEmpty({ message: "Name should not be empty" })
+    name: string;
+
+    @IsEmail({ message: "Email Khong dung dinh dang" })
+    @IsNotEmpty({ message: "Email should not be empty" })
     email: string;
 
     @IsNotEmpty()
     password: string;
 
+    @IsNotEmpty({ message: "Name should not be empty" })
+    age: number;
 
+    @IsNotEmpty({ message: "Gender should not be empty" })
+    gender: string;
+
+    @IsNotEmpty({ message: "Address should not be empty" })
+    address: string;
+
+    @IsNotEmpty({ message: "Role should not be empty" })
+    role: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company!: Company;
+}
+
+export class RegisterUserDto {
+
+    @IsNotEmpty({ message: "Name should not be empty" })
     name: string;
 
+    @IsEmail({ message: "Email Khong dung dinh dang" })
+    @IsNotEmpty({ message: "Email should not be empty" })
+    email: string;
 
+    @IsNotEmpty()
+    password: string;
+
+    @IsNotEmpty({ message: "Name should not be empty" })
+    age: number;
+
+    @IsNotEmpty({ message: "Gender should not be empty" })
+    gender: string;
+
+    @IsNotEmpty({ message: "Address should not be empty" })
     address: string;
 
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,11 +1,14 @@
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
+    @Prop()
+    name: string;
+
     @Prop({ required: true })
     email: string;
 
@@ -13,16 +16,43 @@ export class User {
     password: string;
 
     @Prop()
-    name: string;
-
-    @Prop()
-    phone: string;
-
-    @Prop()
     age: number;
 
     @Prop()
+    gender: number;
+
+    @Prop()
     address: string;
+
+    @Prop({ type: Object })
+    company: {
+        _id: mongoose.Schema.Types.ObjectId,
+        name: string
+    };
+
+    @Prop()
+    role: string;
+
+    @Prop()
+    refreshToken: string;
+
+    @Prop({ type: Object })
+    createdBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
+
+    @Prop({ type: Object })
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
+
+    @Prop({ type: Object })
+    deletedBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
 
 }
 
