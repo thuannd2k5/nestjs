3e9cd906ea7e77688b04c32c58731f976a3da61b
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-25 22:53:38 +0700

    test giao diện part 2 , qua chương tạo module Jobs

--- src/main.ts
+++ src/main.ts
@@ -29,9 +29,10 @@ async function bootstrap() {
   //config cors
   app.enableCors(
     {
-      "origin": "*",
+      "origin": true,
       "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false
+      "preflightContinue": false,
+      credentials: true
     }
   );
 
