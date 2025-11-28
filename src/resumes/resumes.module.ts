ae68f440a58ca5926d9b952a3ce5d5c837f6af21
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-28 16:53:26 +0700

    sửa lỗi code sai phần thêm schema trong import

--- src/resumes/resumes.module.ts
+++ src/resumes/resumes.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ResumesService } from './resumes.service';
 import { ResumesController } from './resumes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Resume } from './schemas/resume.schema';
+import { Resume, ResumeSchema } from './schemas/resume.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Resume.name, schema: Resume }])],
+  imports: [MongooseModule.forFeature([{ name: Resume.name, schema: ResumeSchema }])],
   controllers: [ResumesController],
   providers: [ResumesService]
 })
