bfacee63c27b071d42fc19175722721024f9ed90
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-11-29 02:54:08 +0700

    CRUD permission

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,34 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('permissions')
 export class PermissionsController {
-  constructor(private readonly permissionsService: PermissionsService) {}
+  constructor(private readonly permissionsService: PermissionsService) { }
 
   @Post()
-  create(@Body() createPermissionDto: CreatePermissionDto) {
-    return this.permissionsService.create(createPermissionDto);
+  @ResponseMessage("Create a new permission")
+  async create(@Body() createPermissionDto: CreatePermissionDto, @User() user: IUser) {
+    const permission = await this.permissionsService.create(createPermissionDto, user);
+    return {
+      _id: permission?._id,
+      createdAt: permission?.createdAt
+    }
   }
 
   @Get()
-  findAll() {
-    return this.permissionsService.findAll();
+  @ResponseMessage("Fetch Permission with paginate")
+  async findAll(
+    @Query("current") current: string,
+    @Query("pageSize") pageSize: string,
+    @Query() qs: string,
+  ) {
+    return await this.permissionsService.findAll(+current, +pageSize, qs);
   }
 
   @Get(':id')
+  @ResponseMessage("Fetch Permission by ID")
   findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+    return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  @ResponseMessage("Update a permission")
+  update(
+    @Param('id') id: string,
+    @Body() updatePermissionDto: UpdatePermissionDto,
+    @User() user: IUser) {
+    return this.permissionsService.update(id, updatePermissionDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  @ResponseMessage("Delete a permission")
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.permissionsService.remove(id, user);
   }
 }
